2d92fc1c9a3e60947c5795e96d3ef19756cb07ad
Yuki-yao <yukki2777@hotmail.com>
2018-03-31 17:38:09 +0800

    basic animation

--- Assets/Scripts/KeyboardInput.cs
+++ Assets/Scripts/KeyboardInput.cs
@@ -1,11 +1,51 @@
 using UnityEngine;
+using System;
 using FingerInput;
 
 public class KeyboardInput : IFingerInput{
+  public int[] lasTime;
+  public KeyCode[] keys =
+  {
+    KeyCode.None,
+    KeyCode.LeftShift,
+    KeyCode.W,
+    KeyCode.A,
+    KeyCode.J,
+    KeyCode.D,
+    KeyCode.S,
+    KeyCode.K
+  };
+
+  public KeyboardInput()
+  {
+    lasTime = new int[8];
+    for(int i = 0; i < lasTime.Length; i ++)
+    {
+      lasTime[i] = System.Environment.TickCount;
+    }
+  }
+
   public ActionType GetAction()
   {
-    if (Input.GetKey(KeyCode.D))
-      return ActionType.Forward;
+    for(int i = 1; i < 8; i ++)
+    {
+      if (Input.GetKey(keys[i]))
+      {
+        /*
+        if(i == (int)ActionType.Jump)
+        {
+          if(System.Environment.TickCount - lasTime[i] < 300)
+          {
+            lasTime[i] = System.Environment.TickCount;
+            return ActionType.Null;
+          }
+        }
+        */
+        lasTime[i] = System.Environment.TickCount;
+        return (ActionType)i;
+      }
+    }
+    lasTime[0] = System.Environment.TickCount;
     return ActionType.Null;
   }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,59 @@ public class PlayerController : MonoBehaviour {
 
   private Rigidbody2D body;
   private IFingerInput input;
+  private Animator animator;
+  private float groundY;
   public float speed;
 
 	// Use this for initialization
 	void Start () {
     body = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
     input = new KeyboardInput();
+    groundY = body.position.y;
 	}
 
   void FixedUpdate() {
     Vector2 pos = body.position;
     ActionType at = input.GetAction();
-    if(at == ActionType.Forward)
+    if (pos.y > groundY)
+      return;
+    if (at == ActionType.Forward)
     {
+      animator.SetTrigger("forward");
+      animator.SetBool("isIdle", false);
       Vector2 newpos = new Vector2(pos.x + speed, pos.y);
       body.MovePosition(newpos);
+    }
+    else if (at == ActionType.Backward)
+    {
+      animator.SetTrigger("backward");
+      animator.SetBool("isIdle", false);
+      Vector2 newpos = new Vector2(pos.x - speed, pos.y);
+      body.MovePosition(newpos);
+    }
+    else if (at == ActionType.Jump)
+    {
+      Debug.Log("Jump");
+      animator.SetTrigger("jump");
+      animator.SetBool("isIdle", false);
+      Vector2 upforce = new Vector2(0, 1300);
+      body.AddForce(upforce);
+    }
+    else if (at == ActionType.Squat)
+    {
+      animator.SetTrigger("squat");
+      animator.SetBool("isIdle", false);
+    }
+    else if (at == ActionType.Punch)
+    {
+      animator.SetTrigger("punch");
+      animator.SetBool("isIdle", false);
+    }
+    else
+    {
+      animator.SetBool("isIdle", true);
+      Debug.Log("idle");
     }
   }
 
